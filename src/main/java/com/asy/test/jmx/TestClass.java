192c5bfb7be4cbc256e62168c066521d16a2e82f
asyard <asyard@gmail.com>
2016-02-24 12:52:28 +0200

    Initial commit

---
+++ src/main/java/com/asy/test/jmx/TestClass.java
@@ -0,0 +1,18 @@
+package com.asy.test.jmx;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by asy
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        for(;;) {
+
+        }
+    }
+
+}
---
+++ src/main/java/com/asy/test/jmx/mbean/AppStats.java
@@ -0,0 +1,50 @@
+package com.asy.test.jmx.mbean;
+
+/**
+ * Created by asy
+ */
+public class AppStats {
+
+    private String appDescription;
+    private int maxAllowedConnections;
+    private int currentConnectionCount;
+
+    public int getCurrentConnectionCount() {
+        return currentConnectionCount;
+    }
+
+    public void setAppDescription(String desc) {
+        appDescription = desc;
+    }
+
+    public String getAppDescription() {
+        return appDescription;
+    }
+
+    public int getMaxAllowedConnections() {
+        return maxAllowedConnections;
+    }
+
+    public void setMaxAllowedConnections(int maxAllowedConnections) {
+        if (maxAllowedConnections < 1) {
+            throw new RuntimeException("Max allowed connections cannot be less than 1");
+        }
+        this.maxAllowedConnections = maxAllowedConnections;
+    }
+
+    public void connect() {
+        if (currentConnectionCount >= maxAllowedConnections) {
+            throw new RuntimeException("Max allowed connections exceeded");
+        }
+        currentConnectionCount++;
+    }
+
+    public void disconnect() {
+        currentConnectionCount--;
+    }
+
+    public void releaseAllConnections() {
+        currentConnectionCount = 0;
+    }
+
+}
